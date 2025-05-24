55c22f3cd44d2f62957bd5d1dfa7b6021ef0024e
zakumann <samuel742able@gmail.com>
2025-05-25 00:43:46 +0900

    First Commit

---
+++ Source/ArchAngel/Private/PlayerCharacter.cpp
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputAction.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
+    CameraBoom->TargetArmLength = 300.f;
+    CameraBoom->bUsePawnControlRotation = true;
+
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraBoom);
+
+    bUseControllerRotationYaw = true;
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+    if (APlayerController* PC = Cast<APlayerController>(Controller))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+        {
+            Subsystem->AddMappingContext(InputMapping, 0);
+        }
+    }
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    if (UEnhancedInputComponent* Input = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+        Input->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+        Input->BindAction(AimAction, ETriggerEvent::Started, this, &APlayerCharacter::StartAiming);
+        Input->BindAction(AimAction, ETriggerEvent::Completed, this, &APlayerCharacter::StopAiming);
+        Input->BindAction(FireAction, ETriggerEvent::Started, this, &APlayerCharacter::Fire);
+        Input->BindAction(SlowMoAction, ETriggerEvent::Started, this, &APlayerCharacter::StartSlowMo);
+    }
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+    FVector2D Movement = Value.Get<FVector2D>();
+    AddMovementInput(GetActorForwardVector(), Movement.Y);
+    AddMovementInput(GetActorRightVector(), Movement.X);
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+    FVector2D LookAxis = Value.Get<FVector2D>();
+    AddControllerYawInput(LookAxis.X);
+    AddControllerPitchInput(LookAxis.Y);
+}
+
+void APlayerCharacter::StartAiming()
+{
+    bIsAiming = true;
+    CameraBoom->TargetArmLength = 200.f;
+}
+
+void APlayerCharacter::StopAiming()
+{
+    bIsAiming = false;
+    CameraBoom->TargetArmLength = 300.f;
+}
+
+void APlayerCharacter::Fire()
+{
+    // Call weapon class Fire() here
+}
+
+void APlayerCharacter::StartSlowMo()
+{
+    float CurrentDilation = UGameplayStatics::GetGlobalTimeDilation(this);
+    UGameplayStatics::SetGlobalTimeDilation(this, (CurrentDilation < 1.f) ? 1.f : 0.25f);
+}
+
---
+++ Source/ArchAngel/Public/PlayerCharacter.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class ARCHANGEL_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    void Move(const FInputActionValue& Value);
+    void Look(const FInputActionValue& Value);
+    void StartAiming();
+    void StopAiming();
+    void Fire();
+    void StartSlowMo();
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    class USpringArmComponent* CameraBoom;
+
+    UPROPERTY(VisibleAnywhere)
+    class UCameraComponent* FollowCamera;
+
+    UPROPERTY(EditDefaultsOnly, Category = Input)
+    class UInputMappingContext* InputMapping;
+
+    UPROPERTY(EditDefaultsOnly, Category = Input)
+    class UInputAction* MoveAction;
+
+    UPROPERTY(EditDefaultsOnly, Category = Input)
+    class UInputAction* LookAction;
+
+    UPROPERTY(EditDefaultsOnly, Category = Input)
+    class UInputAction* AimAction;
+
+    UPROPERTY(EditDefaultsOnly, Category = Input)
+    class UInputAction* FireAction;
+
+    UPROPERTY(EditDefaultsOnly, Category = Input)
+    class UInputAction* SlowMoAction;
+
+    bool bIsAiming;
+};
